1b511a4c7aaa973767f13aa0aecefc662ef35c6d
eddiebeherano <eddiebeherano@gmail.com>
2015-09-27 18:39:23 +0200

    update

---
+++ appdeploy.R
@@ -0,0 +1,12 @@
+install.packages('devtools')
+
+devtools::install_github('rstudio/shinyapps')
+
+shinyapps::setAccountInfo(name='edwardbeherano',
+                          token='BB563B8251D4CAC9B1301AADF3F2A55E',
+                          secret='<SECRET>')
+
+library(shinyapps)
+
+deployApp(appName = "Eddie app")
+shinyapps::deployApp('')
